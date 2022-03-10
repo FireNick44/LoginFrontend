ea1353b1986b22317a61aea088373e0a4ee73c32
FireNick44 <yannic.studer@protonmail.com>
2022-03-10 01:43:36 +0100

    error lol gotta sleep now

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ var content = document.querySelector(".content")
 window.addEventListener("load", vanish);
 
 function Sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 async function vanish() {
-    await Sleep(2000);
-    loader.classList.add("disappear");
-    await Sleep(100);
-    content.classList.add("appear");
+  await Sleep(2000);
+  loader.classList.add("disappear");
+  await Sleep(100);
+  content.classList.add("appear");
 }
 
-
-var wrapperMenu = document.querySelector('.wrapper-menu');
+var menu = document.querySelector('.wrapper-menu');
 
 wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');  
+  wrapperMenu.classList.toggle('open');
+  menu;
 })
+
+var test1 = true;
+
+function menu(){
+  if(test1 == true){
+    test4.classList.add("disappear");
+    test4.classList.remove("appear")
+    test1 = false;
+    console.log("dwadawd");
+  }
+  else if(test1 == false){
+    test4.classList.add("appear");
+    test4.classList.remove("disappear")
+    test1 = true;
+  }
+}
