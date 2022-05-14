6c94f567c165be8f77326b72e57be36fb356e97c
FireNick44 <yannic.studer@protonmail.com>
2022-05-14 23:36:22 +0200

    Final Beta v1.0 Update; + dark/lightmode saving + cleanup + bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SecureLogin IPT 6.0 Projekt
+
+known issue: not correctly working on edge, internet explorer
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var loader = document.querySelector(".loader")
-var content = document.querySelector(".content")
+var loader = document.querySelector(".loader");
+var content = document.querySelector(".content");
 
 window.addEventListener("load", sessionLoadPage);
 
@@ -10,6 +10,8 @@ function Sleep(milliseconds) {
 
 //PageLoader
 async function sessionLoadPage() {
+  if(sessionStorage.getItem("pageHasLightmode", true)) onModemenuKlick(); //looking for lightmode
+
   if(!sessionStorage.getItem("pageWasLoaded")) {
       loader.classList.add("appear");
       await Sleep(3000);
@@ -28,49 +30,50 @@ async function sessionLoadPage() {
 
 //Darkmode & Lightmode
 var body = document.body;
-
 var modemenu = document.querySelector(".cardmode");
-
-if (modemenu) {
-  modemenu.addEventListener("click", function(){
-    modemenu.classList.toggle("open");
-    body.classList.toggle("body-light");
-    bg.classList.toggle("background-light");
-    lnk.classList.toggle("lnk-light");
-    lnk1.classList.toggle("lnk-light");
-    cards.classList.toggle("card-light");
-    cards1.classList.toggle("card-light");
-    
-    mail.classList.toggle("mail-light");
-    pw.classList.toggle("pw-light");
-  
-    username.classList.toggle("username-light");
-    pwrep.classList.toggle("pw-rep-light");
-  });
-  
-}
 var bg = document.querySelector(".background");
 var lnk = document.querySelector(".navlink.lnk1");
 var lnk1 = document.querySelector(".navlink.lnk2");
 var cards = document.querySelector(".card.cardpw");
 var cards1 = document.querySelector(".card.cardmode");
+var cards2 = document.querySelector(".card.usercard");
 var username = document.querySelector(".username");
 var mail = document.querySelector(".mail");
 var pw = document.querySelector(".pw");
 var pwrep = document.querySelector(".pw-rep");
 
 function onModemenuKlick(){
+  if(modemenu.classList.contains("open")) sessionStorage.setItem("pageHasLightmode", true); //lightmode
+  else if(!modemenu.classList.contains("open")) sessionStorage.removeItem("pageHasLightmode") //Darkmode
+  
   modemenu.classList.toggle("open");
   body.classList.toggle("body-light");
   bg.classList.toggle("background-light");
-  lnk.classList.toggle("lnk-light");
-  lnk1.classList.toggle("lnk-light");
-  cards.classList.toggle("card-light");
   cards1.classList.toggle("card-light");
+
+  if(cards2 == null){
+    lnk.classList.toggle("lnk-light"); //null bei user
+    lnk1.classList.toggle("lnk-light"); //null bei user
+    cards.classList.toggle("card-light");
+    mail.classList.toggle("mail-light"); //null bei user
+    pw.classList.toggle("pw-light"); //null bei user
+  }
+
+  if(username != null){
+    username.classList.toggle("username-light");
+    pwrep.classList.toggle("pw-rep-light");
+  }
+
+  if(cards2 != null) cards2.classList.toggle("card-light"); //null bei nicht user
+}
+
+function onModemenuKlickUser(){
+  if(modemenu.classList.contains("open")) sessionStorage.setItem("pageHasLightmode", true); //lightmode
+  else if(!modemenu.classList.contains("open")) sessionStorage.removeItem("pageHasLightmode") //Darkmode
   
-  mail.classList.toggle("mail-light");
-  pw.classList.toggle("pw-light");
-  
-  username.classList.toggle("username-light");
-  pwrep.classList.toggle("pw-rep-light");
+  modemenu.classList.toggle("open");
+  body.classList.toggle("body-light");
+  bg.classList.toggle("background-light");
+  cards1.classList.toggle("card-light");
+  cards2.classList.toggle("card-light");
 }
